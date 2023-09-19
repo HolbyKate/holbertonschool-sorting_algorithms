04bd72c0c80747838f9da6a300e4ecb4c2343394
JeremMac <macal.jerem@gmail.com>
2023-09-19 14:36:11 +0200

    adding the insertion sort list function

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *my_list = NULL;
+	listint_t *swap_list = NULL;
+
+	if (list == NULL || *list == NULL)
+	{
+		return;
+	}
+	my_list = *list;
+
+	while (my_list != NULL)
+	{
+		swap_list = my_list;
+		while (swap_list->prev != NULL && swap_list->prev->n > swap_list->n)
+		{
+			if (swap_list->next == NULL)
+			{
+				swap_list->prev->next = NULL;
+			}
+			else
+			{
+				swap_list->next->prev = swap_list->prev;
+				swap_list->prev->next = swap_list->next;
+			}
+			swap_list->next = swap_list->prev;
+			swap_list->prev = swap_list->prev->prev;
+			swap_list->next->prev = swap_list;
+			if (swap_list->prev == NULL)
+			{
+				*list = swap_list;
+			}
+			else
+			{
+				swap_list->prev->next = swap_list;
+			}
+			print_list(*list);
+		}
+		my_list = my_list->next;
+	}
+}
