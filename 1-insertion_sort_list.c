49f63dd7137deeb3c563c2479cd45b6b702a81dd
JeremMac <macal.jerem@gmail.com>
2023-09-19 14:52:40 +0200

    Correcting NULL case condition.

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *my_list = NULL;
 	listint_t *swap_list = NULL;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
